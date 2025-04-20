5b03d95878bcd25cfd3732dc00e09dc618f891d4
mr.ahner <tommy.james14@proton.me>
2025-04-20 07:45:04 -0700

    change eveerytig
    fix

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <?php
-//abscissa software
-//ability to upload a shipwreck with a article about it, title and image, username, and date. Currently, let's just do title, article, and date. and basic username
-//style it
+    require("ip.php");
+    require("db.php");
+      $sql = "SELECT * FROM articles ORDER BY date DESC";
+      $result = $conn->query($sql);
 
-$sql = "SELECT * FROM articles ORDER BY date DESC";
-$result = $conn->query($sql);
-$row = $result->fetch_assoc();
-if ($result->num_rows > 0) {
-    $username = htmlspecialchars($row['username']);
-    echo htmlspecialchars("<h1>" . $row['title'] . "</h1>");
-    echo htmlspecialchars("<p>" . $row['username'] . "</p>");
-    echo htmlspecialchars("<small>$username posted on: " . $row['date'] . "</small>");
-    echo "<hr>";
-
-}
+      if ($result->num_rows > 0) {
+          while ($row = $result->fetch_assoc()) {
+              $title = htmlspecialchars($row['title']);
+              $id = (int)$row['id'];
+              echo "<a href='view.php?id=$id'>&bull; $title</a>";
+          }
+      } else {
+          echo "<p>No posts!</p>";
+      }
 ?>
