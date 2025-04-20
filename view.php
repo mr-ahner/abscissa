326d3caa7953b3cfa8413007b7ef5ddb25d56264
mr.ahner <tommy.james14@proton.me>
2025-04-20 07:47:54 -0700

    Update view.php

--- view.php
+++ view.php
@@ -1,4 +1,45 @@
 <?php
+require ("db.php");
+require("ip.php");
+
+      $sql = "SELECT * FROM articles ORDER BY date DESC";
+      $result = $conn->query($sql);
+
+      if ($result->num_rows > 0) {
+          while ($row = $result->fetch_assoc()) {
+              $title = htmlspecialchars($row['title']);
+              $id = (int)$row['id'];
+              echo "<a href='view.php?id=$id'>&bull; $title</a>";
+          }
+      } else {
+          echo "<p>No posts!</p>";
+      }
+      ?>
+ 
 
 
-?>
+      <h2><?php $title  ?></h2>
+      <?php
+      $id = $_GET['id'];
+
+        $sql = "SELECT * FROM articles WHERE id = $id LIMIT 1";
+        
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+            $title = htmlspecialchars($row['title']);
+            $username = htmlspecialchars($row['username']);
+            $date = htmlspecialchars($row['date']);
+            $article = nl2br(htmlspecialchars($row['content'])); 
+        
+            echo ("<h1>$title</h1>");
+            echo ("<p>$username</p>");
+            echo ("<p>$article</p>");
+            echo ("<small>$date</small>");
+
+        } 
+        
+        }else {
+            echo "not a true id";
+        }
+      ?>
